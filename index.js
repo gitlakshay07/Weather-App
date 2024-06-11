2407faeda094502b88a9b9cc64becdc6f878a00c
gitlakshay10 <lakshay@lancepaas.com>
2024-06-11 11:34:54 +0530

    11-commit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-let d = new Date;
 
-const dateBuilder = (d) => {
+
+const dateBuilder = () => {
+    let d = new Date;
+
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   
@@ -8,37 +10,60 @@ const dateBuilder = (d) => {
     let date = d.getDate();
     let month = months[d.getMonth()];
     let year = d.getFullYear();
-    const hour = d.getHours();
-    const minuts = d.getMinutes();
-  
-    return `${hour}:${minuts} ${day}, ${date} ${month} ${year}`;
-};
+    let hour = d.getHours();
+    let minuts = d.getMinutes();
+    let seconds = d.getSeconds();
 
+    minuts = minuts < 10 ? '0' + minuts : minuts;
+  
+    return `${hour}:${minuts}:${seconds} ${day}, ${date} ${month} ${year}`;
+};
 $(document).ready(function(){
 
-    let apiKey = '320cbe9c0b7fc6c430d04127cfe24661'
-    let city = 'jaipur'
+    let apiKey = '320cbe9c0b7fc6c430d04127cfe24661';
+    let city = 'New Delhi';
 
-    $.ajax({
-        url : `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`,
-        type : 'GET',
-        dataType : 'json',
-        success : function(data){
+    function fetchWeather(city){
+        $.ajax({
+            url : `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`,
+            type : 'GET',
+            dataType : 'json',
+            success : function(data){
 
-            $(".city-name").text(data.name);
-            $(".temprat").text(data.main.temp + "°c");
-            $(".condition").text(data.weather[0].description)
-            $(".wind").text(data.wind.speed + "Km/h")
-            $(".humidity").text(data.main.humidity+"%")
-            $(".cloud").text(data.clouds.all+"%")
+                $('.weather-icon').html("<img class='text-center' src='https://openweathermap.org/img/wn/"+data.weather[0].icon+"@2x.png' height='50px' width='50px'>")
+    
+                $(".city-name").text(data.name);
+                $("#country").text(data.sys.country)
+                $(".temprat").text(data.main.temp + "°");
+                $(".condition").text(data.weather[0].description)
+                $(".wind").text(data.wind.speed + "Km/h")
+                $(".humidity").text(data.main.humidity+"%")
+                $(".cloud").text(data.clouds.all+"%")
+                $(".visiblity").text(data.visibility+"%")
+    
+            },
+            error: function(error) {
+                console.log('Error:', error);
+                // alert('Failed to fetch weather data. Please check the city name and try again.');
+            }
+        })
+    };
+    fetchWeather(city);
 
-        },
-        error: function(error) {
-            console.log('Error:', error);
+    setInterval(() => {
+        fetchWeather(city);
+    }, 6000);
+
+    $(".time-date").text(dateBuilder());
+    setInterval(() => {
+        $(".time-date").text(dateBuilder());
+    }, 1000);
+
+    $("#locatn-inp").on('submit', function(e) {
+        e.preventDefault();
+        city = $(".search").val();
+        if (city) {
+            fetchWeather(city);
         }
-    })
-
-    $(".time-date").text(dateBuilder(d))
+    });
 })
-
-
